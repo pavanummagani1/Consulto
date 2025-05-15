f9124d3c6e0f166c8ef3cefd828f04f796db6596
PavanUmmagani07 <pavankalyanU@outlook.com>
2025-05-15 15:34:45 +0530

    deployment

--- Frontend/src/Pages/client/profilePage.jsx
+++ Frontend/src/Pages/client/profilePage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
-import '../../Styles/client/ProfilePage.css';
+import '../../Styles/client/profilePage.css'
 
 const ProfilePage = () => {
   const [showPassword, setShowPassword] = useState(false);
