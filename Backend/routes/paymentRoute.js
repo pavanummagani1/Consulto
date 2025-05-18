12329a0d707684c2cb0cbead08b527ee42194909
PavanUmmagani07 <pavankalyanU@outlook.com>
2025-05-18 19:14:25 +0530

    Payment

--- Backend/routes/paymentRoute.js
+++ Backend/routes/paymentRoute.js
@@ -1,10 +1,11 @@
 // paymentRoute.js
 import express from 'express';
 import { createPaymentIntent, handleWebhook } from '../Controllers/paymentController.js';
+import { verifyToken } from '../utils/utility.js';
 
 const paymentRouter = express.Router();
 
-paymentRouter.post('/create-payment-intent', createPaymentIntent);
+paymentRouter.post('/create-payment-intent', verifyToken, createPaymentIntent);
 paymentRouter.post('/webhook', express.raw({type: 'application/json'}), handleWebhook);
 
 export default paymentRouter;
--- Backend/utils/utility.js
+++ Backend/utils/utility.js
@@ -23,3 +23,22 @@ export const generateOtp = () => {
     const otp = Math.floor(100000 + Math.random() * 900000);
     return otp
 }
+
+
+// verify the token
+export const verifyToken = (req, res, next) => {
+  const token = req.headers['authorization']?.split(' ')[1]; // Extract token from "Bearer <token>"
+  
+  if (!token) {
+    return res.status(403).json({ error: "No token provided" });
+  }
+
+  jwt.verify(token, process.env.LOGIN_SECRET_KEY, (err, decoded) => {
+    if (err) {
+      console.error("Token verification failed:", err);
+      return res.status(401).json({ error: "Invalid or expired token" });
+    }
+    req.user = decoded; // Attach decoded user data to the request
+    next(); // Proceed to the next middleware/route
+  });
+};
